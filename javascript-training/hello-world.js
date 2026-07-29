82f099462944ae5147b9ae553f94340bf21c35c0
Manimozhiyan <manimozhiyan1990@gmail.com>
2026-07-29 08:47:23 +0530

    Assignment 1 - datatypes

---
+++ javascript-training/hello-world.js
@@ -0,0 +1,14 @@
+console.log("manimozhiyan");
+
+console.info("Informed");
+
+console.error("Error message has been Displayed");
+
+console.warn("Warning message Has been Displayed ")
+
+
+/*to always give you written meaning 
+whenever you are going to provide 
+A and B values. Always, it should 
+return back the sum of those two numbers. 
+This is the logic I want to develop.*/
